bd6f3cfac470c803c69a9dee73c55c42e1ffeb9e
Sungmin Yang <smyang@nearthlab.com>
2019-02-04 14:34:52 +0900

    2017 d4

---
+++ src/advent_of_code/y2017/d4.clj
@@ -0,0 +1,21 @@
+(ns advent_of_code.y2017.d4
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input
+  (line-seq (io/reader (io/resource "y2017/d4.input"))))
+
+(defn parse
+  [line]
+  (str/split line #"\s"))
+
+(defn diff-with-map
+  [line]
+  (= (count (into #{} line)) (count line)))
+
+;; p1
+(count
+  (->> input
+     (map parse)
+     (map diff-with-map)
+     (filter true?)))
