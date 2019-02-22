3f72990b7800901e92a33f251b3f8ba0af9d113b
Yang Sungmin <smyang@nearthlab.com>
2019-02-22 18:30:00 +0900

    first-duplicate and reductions

--- src/advent_of_code/y2018/d1.clj
+++ src/advent_of_code/y2018/d1.clj
@@ -19,3 +19,24 @@
     (cycle (map read-string input))))
 
 ; "Elapsed time: 432965.000471 msecs"
+
+; from namenu util.cljc
+; https://github.com/namenu/advent-of-code/blob/master/src/util.cljc
+(defn first-duplicate
+  ([xs]
+   (first-duplicate identity xs))
+  ([key-fn xs]
+   (let [result (reduce (fn [seen x]
+                          (let [k (key-fn x)]
+                            (if (seen k)
+                              (reduced x)
+                              (conj seen k))))
+                        #{} xs)]
+     (if (set? result)
+       nil
+       result))))
+
+(time
+  (first-duplicate (reductions + (cycle (map read-string input)))))
+
+; "Elapsed time: 174.588303 msecs"
