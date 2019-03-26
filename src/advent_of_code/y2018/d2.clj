e35ecd687addf0578ade4f45c53293d3612476a2
Yang Sungmin <smyang@nearthlab.com>
2019-03-26 09:48:53 +0900

    fix some error

--- src/advent_of_code/y2018/d2.clj
+++ src/advent_of_code/y2018/d2.clj
@@ -1,8 +1,7 @@
 ; --- Day 2: Inventory Management System ---
 ; https://adventofcode.com/2018/day/2
 (ns advent_of_code.y2018.d2
-  (:require [clojure.java.io :as io]
-             [clojure.data]))
+  (:require [clojure.java.io :as io]))
 
 (def input
   (line-seq (io/reader (io/resource "y2018/d2.input"))))
@@ -14,44 +13,16 @@
 (def two-times? (n-times? 2))
 (def three-times? (n-times? 3))
 
-(defn inc-if-true
-  [acc val]
+(defn inc-true [acc val]
   (if (true? val) (inc acc) acc))
 
-(defn inc-vec-if-true
-  [acc val]
-  [(inc-if-true (first acc) (first val))
-   (inc-if-true (second acc) (second val))])
+(defn inc-true-vec [acc val]
+  [(inc-true (first acc) (first val))
+   (inc-true (second acc) (second val))])
 
 ; p1
 (->> input
      (map #(frequencies (seq %)))
      (map (juxt two-times? three-times?))
-     (reduce inc-vec-if-true [0 0])
+     (reduce inc-true-vec [0 0])
      (reduce *))
-
-;
-; p2 TODO
-;
-;(defn diff-str
-;  [str1 str2]
-;  )
-;
-;(defn correct?
-;  [str1 str2]
-;  (= (count (diff-str str1 str2)) 1))
-;
-;(defn has-correct?
-;  [str coll]
-;  (filter #(correct? str %) coll))
-;
-;(defn map-correct?
-;  [coll]
-;  (map #(has-correct? % coll) coll))
-;
-;; p2
-;(reduce
-;  (fn [result item])
-;  ""
-;  input)
-;
