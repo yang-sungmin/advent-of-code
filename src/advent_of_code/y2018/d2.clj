ccd1adc46a02b29f936c6f4a9f5be68c9433efc3
Sungmin Yang <smyang@nearthlab.com>
2019-03-26 22:13:33 +0900

    Update d2.clj

--- src/advent_of_code/y2018/d2.clj
+++ src/advent_of_code/y2018/d2.clj
@@ -26,3 +26,25 @@
      (map (juxt two-times? three-times?))
      (reduce inc-true-vec [0 0])
      (reduce *))
+
+; p2
+(defn count-n-diff [n str1 str2]
+  (reduce
+    (fn [acc item]
+      (if (not= (first item) (second item))
+        (inc acc)
+        acc))
+    0
+    (partition n (interleave str1 str2))))
+
+(defn get-n-diff [n str coll]
+  (remove false? (keep #(if (= (count-n-diff n str %) 1) % false) coll)))
+
+(defn find-n-diff [n coll]
+  (map #(get-n-diff n % coll) coll))
+
+(defn correct?
+  [input]
+  (apply str (map first (filter #(= (first %) (second %)) (partition 2 (apply interleave (flatten (find-n-diff 2 input))))))))
+
+(correct? input)
